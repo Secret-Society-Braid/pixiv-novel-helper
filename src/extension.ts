86f501e8e9c4d670d94048daa0659cad6c9a65f5
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-13 18:06:26 +0900

    add commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,68 @@
 import * as vscode from 'vscode';
+import Location from './Location';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "pixiv-novel-helper" is now active!');
+	console.log('Activated "pixiv-novel-helper"!');
 
-	const disposable = vscode.commands.registerCommand('pixiv-novel-helper.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello VScode World! from pixiv-novel-helper!');
+	const location: Location = new Location();
+
+	// open pixiv home page
+	const openPixivCommand = vscode.commands.registerCommand('pixiv-novel-helper.openPixiv', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct({ relativePath: undefined })));
 	});
 
-	context.subscriptions.push(disposable);
+	// open dashboard related pages
+	const openPixivDashBoardHome = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardHome', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard' ] } )));
+	});
+
+	const openPixivDashBoardAllHome = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardAllHome', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works' ] } )));
+	});
+
+	const openPixivDashBoardAllSeries = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardAllSeries', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works' ,'series' ] } )));
+	});
+
+	const openPixivDashBoardAllDrafts = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardAllDrafts', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works', 'drafts' ] } )));
+	});
+
+	const openPixivDashBoardIllustrationsHome = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardIllustrationsHome', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works', 'illustrations' ] } )));
+	});
+
+	const openPixivDashBoardNovelsHome = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardNovelsHome', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works', 'novels' ] } )));
+	});
+
+	const openPixivDashBoardNovelsSeries = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardNovelsSeries', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works', 'novels', 'series' ] } )));
+	});
+
+	const openPixivDashBoardNovelsDrafts = vscode.commands.registerCommand('pixiv-novel-helper.openPixivDashBoardNovelsDrafts', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'dashboard', 'works', 'novels', 'drafts' ] } )));
+	});
+
+	// open request related pages
+	const openPixivRequestHome = vscode.commands.registerCommand('pixiv-novel-helper.openPixivRequestHome', () => {
+		vscode.env.openExternal(vscode.Uri.parse(location.construct( { relativePath: [ 'manage', 'requests' ] } )));
+	});
+
+	// register to the subscriptions
+	context.subscriptions.push(
+		openPixivCommand,
+		openPixivDashBoardHome,
+		openPixivDashBoardAllHome,
+		openPixivDashBoardAllSeries,
+		openPixivDashBoardAllDrafts,
+		openPixivDashBoardIllustrationsHome,
+		openPixivDashBoardNovelsHome,
+		openPixivDashBoardNovelsSeries,
+		openPixivDashBoardNovelsDrafts,
+		openPixivRequestHome
+	);
 }
 
 export function deactivate() {}
