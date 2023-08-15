237b910d9f4c41a2fab970f9b9e74ca165612300
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-15 10:05:15 +0900

    format file

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,47 @@
-import * as vscode from 'vscode';
-import {CharacterCount, CharacterCounterController} from "./features/characterCount";
-import { openPixivCommand, openPixivDashBoardAllDrafts, openPixivDashBoardAllHome, openPixivDashBoardAllSeries, openPixivDashBoardHome, openPixivDashBoardIllustrationsHome, openPixivDashBoardNovelsDrafts, openPixivDashBoardNovelsHome, openPixivDashBoardNovelsSeries, openPixivRequestHome, openPixivUploadPage } from './features/commands';
+import * as vscode from "vscode";
+import {
+  CharacterCount,
+  CharacterCounterController,
+} from "./features/characterCount";
+import {
+  openPixivCommand,
+  openPixivDashBoardAllDrafts,
+  openPixivDashBoardAllHome,
+  openPixivDashBoardAllSeries,
+  openPixivDashBoardHome,
+  openPixivDashBoardIllustrationsHome,
+  openPixivDashBoardNovelsDrafts,
+  openPixivDashBoardNovelsHome,
+  openPixivDashBoardNovelsSeries,
+  openPixivRequestHome,
+  openPixivUploadPage,
+} from "./features/commands";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Activated "pixiv-novel-helper"!');
 
-	console.log('Activated "pixiv-novel-helper"!');
+  // character count initialization
+  const characterCount = new CharacterCount();
+  const controller = new CharacterCounterController(characterCount);
 
-	// character count initialization
-	const characterCount = new CharacterCount();
-	const controller = new CharacterCounterController(characterCount);
-
-	// register to the subscriptions
-	context.subscriptions.push(
-		// commands
-		openPixivCommand,
-		openPixivDashBoardHome,
-		openPixivDashBoardAllHome,
-		openPixivDashBoardAllSeries,
-		openPixivDashBoardAllDrafts,
-		openPixivDashBoardIllustrationsHome,
-		openPixivDashBoardNovelsHome,
-		openPixivDashBoardNovelsSeries,
-		openPixivDashBoardNovelsDrafts,
-		openPixivRequestHome,
-		openPixivUploadPage,
-		// character count
-		controller,
-		characterCount
-	);
+  // register to the subscriptions
+  context.subscriptions.push(
+    // commands
+    openPixivCommand,
+    openPixivDashBoardHome,
+    openPixivDashBoardAllHome,
+    openPixivDashBoardAllSeries,
+    openPixivDashBoardAllDrafts,
+    openPixivDashBoardIllustrationsHome,
+    openPixivDashBoardNovelsHome,
+    openPixivDashBoardNovelsSeries,
+    openPixivDashBoardNovelsDrafts,
+    openPixivRequestHome,
+    openPixivUploadPage,
+    // character count
+    controller,
+    characterCount
+  );
 }
 
 export function deactivate() {}
